5350fe1e758c1dff14652f0fceb0e7c2e4ffb90b
hectorbertucat <hectorbertucat@gmail.com>
2024-01-14 23:09:24 +0000

    Add ThingSpeak MQTT functionality for publishing distance measurements

--- main.py
+++ main.py
@@ -2,31 +2,32 @@ import time
 import pycom
 from network import WLAN
 import machine
-import os
+from mqtt import MQTTClient
 
-print("Starting the program...")
+# ----- Define Variables -----
 
-pycom.heartbeat(False)
-
-# Define the network credentials
+# Network Credentials
 WIFI_SSID = "Vodafone-2998D2"
 PASSWORD = "GMkDV28KjDABFh37"
 
-# Define the pins for TRIG and ECHO
+# Define ThingSpeak variables
+THINGSPEAK_API_KEY = '74Z2BWQHJHOPFQCG'  # Replace with your ThingSpeak API key
+THINGSPEAK_BASE_URL = 'https://api.thingspeak.com/update'
+THINGSPEAK_CHANNEL_ID = '2402760'
+THINGSPEAK_CLIENT_ID = 'GRwEKA8PGiAkDAMNEQoiFT0'
+THINGSPEAK_MQTT_HOST = 'mqtt3.thingspeak.com'
+THINGSPEAK_MQTT_PASSWORD = 'U0rTBfXygeELaHRtv7Pn4zEw'
+THINGSPEAK_PUB_FIELD = 'field1'
+
+last_distance = None
+
+# ----- Define Pins -----
+
+# Define the pins for TRIG and ECHO (Ultrasonic Sensor)
 trig = machine.Pin('P12', mode=machine.Pin.OUT)
 echo = machine.Pin('P11', mode=machine.Pin.IN)
 
-# Initialize the WiFi interface
-wlan = WLAN(mode=WLAN.STA)
-
-wlan.connect(ssid=WIFI_SSID, auth=(WLAN.WPA2, PASSWORD))
-
-# Wait until the device is connected to the WiFi network
-while not wlan.isconnected():
-    machine.idle()
-
-# Display a green led when the device is connected to the WiFi network
-pycom.rgbled(0x00FF00)
+# ----- Define Functions -----
 
 def measure_distance():
     # Send a 10us pulse.
@@ -49,10 +50,47 @@ def measure_distance():
 
     return distance
 
+# ----- WiFi Setup -----
+
+# Initialize the WiFi interface and set antenna
+wlan = WLAN(mode=WLAN.STA)
+wlan.antenna(WLAN.EXT_ANT)
+
+if machine.reset_cause() != machine.SOFT_RESET:
+    wlan.init(mode=WLAN.STA)
+    wlan.ifconfig(config=('192.168.1.109', '255.255.255.0', '192.168.1.1', '8.8.8.8'))
+
+# Connect to the WiFi network
+wlan.connect(ssid=WIFI_SSID, auth=(WLAN.WPA2, PASSWORD))
+
+print("Connecting to Wifi...", end='')
+
+# Wait until the device is connected to the WiFi network
+while not wlan.isconnected():
+    print(".", end='')
+    time.sleep(1)
+
+print("\nConnected to Wifi\n")
+print(wlan.ifconfig())
+
+# ----- ThingSpeak Setup (MQTT)-----
+
+print("Connecting to ThingSpeak MQTT....")
+# Define the MQTT client
+client = MQTTClient(THINGSPEAK_CLIENT_ID, "mqtt3.thingspeak.com", port=1883, user=THINGSPEAK_CLIENT_ID, password=THINGSPEAK_MQTT_PASSWORD)
+client.connect()
+
+print("Connected to ThingSpeak MQTT\n")
+
 while True:
+    # Measure the distance
     distance = measure_distance()
-    if distance < 10: # 10cm
-        print("Object is close!")
-    else:
-        print("Object is far away.")
-    time.sleep(1)  # Wait for a second before the next measurement.
+
+    # Print the distance
+    print("Distance: {} cm".format(distance))
+
+    # Publish the distance to ThingSpeak
+    client.publish(topic="channels/{:s}/publish/fields/{:s}".format(THINGSPEAK_CHANNEL_ID, THINGSPEAK_PUB_FIELD), msg=str(distance))
+
+    # Wait 5 seconds
+    time.sleep(5)
