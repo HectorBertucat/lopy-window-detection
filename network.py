7212aec55b790582388e8e1cbeee8c9b18f9bb42
hectorbertucat <hectorbertucat@gmail.com>
2024-01-15 15:28:17 +0000

    Remove network credentials and update network.py

--- network.py
+++ network.py
@@ -7,10 +7,6 @@ STATIC_IP = "192.168.1.100"
 GATEWAY_IP = "192.168.1.1"
 DNS = "8.8.8.8"
 
-# Network Credentials
-WIFI_SSID = "Vodafone-2998D2"
-PASSWORD = "GMkDV28KjDABFh37"
-
 def connect_to_wifi(ssid, password):
     print("Connecting to Wifi...", end='')
     wlan = WLAN(mode=WLAN.STA)
